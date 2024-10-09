33ec10114378a3a3c5037458cbe869dee587c0b4
Emma Konrad <emma.konrad92@gmail.com>
2024-10-09 13:30:32 -0400

    Implemented useEffect on forecast for search, removed imperial conversion

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,45 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("celsius");
-  const [temperature, setTemperature] = useState(props.celsius);
-
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setTemperature((props.celsius * 9) / 5 + 32);
-    setUnit("fahrenheit");
-  }
-
-  function showCelsius(event) {
-    event.preventDefault();
-    setTemperature(props.celsius);
-    setUnit("celsius");
-  }
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(temperature)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(temperature)}</span>
-        <span className="unit">
-          <a href="/" onClick={showCelsius}>
-            °C{" "}
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
